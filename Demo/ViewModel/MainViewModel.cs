6d22d2a8c1556d104bc42d965627868f878054a4
aanshubham <sharma.shubham137@gmail.com>
2024-02-16 20:38:31 +0530

    Added Detail page

---
+++ Demo/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace Demo.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    public MainViewModel()
+    {
+        Items = new ObservableCollection<string>();
+    }
+
+    [ObservableProperty]
+    ObservableCollection<string> items;
+
+    [ObservableProperty]
+    string text;
+
+    // Add button add Items
+    [RelayCommand]
+    void Add()
+    {
+        if (string.IsNullOrWhiteSpace(Text))
+            return;
+
+        Items.Add(Text);
+        // Add Ouer Item
+        Text = string.Empty; 
+    }
+
+    // Swip Right to Delte Item
+    [RelayCommand]
+    void Delete(string s)
+    {
+        if (Items.Contains(s))
+        {
+            Items.Remove(s);
+        }
+    }
+
+    [RelayCommand]
+    async Task Tap(string s)
+    {
+        await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+    }
+}
